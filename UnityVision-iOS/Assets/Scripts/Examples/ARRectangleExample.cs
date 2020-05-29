8aa309543b753228c7f12a7500f669891662f4ef
Tamas Losonczi <losonczi.tamas91@gmail.com>
2020-05-29 15:04:17 +0200

    [sample updates] finish up AR rect sample

--- UnityVision-iOS/Assets/Scripts/Examples/ARRectangleExample.cs
+++ UnityVision-iOS/Assets/Scripts/Examples/ARRectangleExample.cs
@@ -21,6 +21,8 @@ namespace Examples
 	/// </summary>
 	public class ARRectangleExample : MonoBehaviour 
 	{
+		private readonly Vector2 ScreenDimensions = new Vector2(Screen.width, Screen.height);
+		
 		[SerializeField] private ARCameraManager _cameraManager;
 		[SerializeField] private Vision _vision;
 		[SerializeField] private RectangleMarker _rectangleMarkerPrefab;
@@ -86,103 +88,43 @@ namespace Examples
 			public Vector3 bottomRight;
 		}
 
-		private bool HitTestForRect(VisionRectangle rectangle, out RectangleHit result)
+		private bool HitTestForRect(VisionRectangle visionRect, out RectangleHit result)
 		{
 			var hits = new List<ARRaycastHit>();
-			result = null;
 			
-			Vector3 topLeft;
-			if (_raycastManager.Raycast(rectangle.topLeft, hits, TrackableType.Planes))
+			// Vision rect coordinates are normalized, and they come in a reversed order.
+			var topLeft = Vector3.Scale(visionRect.bottomRight, ScreenDimensions);
+			var bottomLeft = Vector3.Scale(visionRect.topRight, ScreenDimensions);
+			var topRight = Vector3.Scale(visionRect.bottomLeft, ScreenDimensions);
+			var bottomRight = Vector3.Scale(visionRect.topLeft, ScreenDimensions);
+			
+			result = new RectangleHit();
+			
+			if (_raycastManager.Raycast(topLeft, hits, TrackableType.Planes))
 			{
-				topLeft = hits.First().pose.position;
+				result.topLeft = hits.First().pose.position;
 			}
 			else return false;
 
-			Vector3 topRight;
-			if (_raycastManager.Raycast(rectangle.topRight, hits, TrackableType.Planes))
+			if (_raycastManager.Raycast(topRight, hits, TrackableType.Planes))
 			{
-				topRight = hits.First().pose.position;
+				result.topRight = hits.First().pose.position;
 			}
 			else return false;
 			
-			Vector3 bottomLeft;
-			if (_raycastManager.Raycast(rectangle.bottomLeft, hits, TrackableType.Planes))
+			if (_raycastManager.Raycast(bottomLeft, hits, TrackableType.Planes))
 			{
-				bottomLeft = hits.First().pose.position;
+				result.bottomLeft = hits.First().pose.position;
 			} 
 			else return false;
 			
-			Vector3 bottomRight;
-			if (_raycastManager.Raycast(rectangle.bottomRight, hits, TrackableType.Planes))
+			if (_raycastManager.Raycast(bottomRight, hits, TrackableType.Planes))
 			{
-				bottomRight = hits.First().pose.position;
+				result.bottomRight = hits.First().pose.position;
 			} 
 			else return false;
-
-			result = new RectangleHit
-			{
-				topLeft = topLeft, 
-				bottomLeft = bottomLeft, 
-				topRight = topRight, 
-				bottomRight = bottomRight
-			};
 
 			return true;
-		}
-
-		private Vector2 topLeft = new Vector2(0.3f, 0.7f);
-		private Vector2 bottomLeft = new Vector2(0.3f, 0.3f);
-		private Vector2 topRight = new Vector2(0.8f, 0.7f);
-		private Vector2 bottomRight = new Vector2(0.8f, 0.3f);
-		
-		private readonly Vector2 ScreenDimensions = new Vector2(Screen.width, Screen.height);
-		
-		void OnGUI() 
-		{
-			GUI.color = Color.blue;
-
-			GUI.Label(new Rect(FlipVertically(topLeft), new Vector2(100, 100)), "topleft");
-			GUI.Label(new Rect(FlipVertically(bottomLeft), new Vector2(100, 100)), "bottomleft");
-			GUI.Label(new Rect(FlipVertically(topRight), new Vector2(100, 100)), "topright");
-			GUI.Label(new Rect(FlipVertically(bottomRight), new Vector2(100, 100)), "bottomright");
-			GUI.Label(new Rect(FlipVertically(_touchPosition), new Vector2(100, 100)), "touchPosition");
-		}
-
-		private GameObject _topLeftMarker;
-		private GameObject _topRightMarker;
-		private GameObject _bottomLeftMarker;
-		private GameObject _bottomRightMarker;
-		
-		private GameObject _touchMarker;
-
-		private Vector2 _touchPosition;
-
-		private void Update()
-		{
-			if (Input.touchCount > 0)
-			{
-				var hits = new List<ARRaycastHit>();
-				_touchPosition = Input.GetTouch(0).position;
-				
-				if (_raycastManager.Raycast(_touchPosition, hits, TrackableType.Planes))
-				{
-					var result = hits.First();
-					if (_touchMarker == null)
-					{
-						_touchMarker = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-					}
-
-					_touchMarker.transform.localScale = Vector3.one * 0.1f;
-					_touchMarker.transform.position = result.pose.position;
-					_touchMarker.transform.rotation = result.pose.rotation;
-					Debug.Log("Touch marker pos" + _touchMarker.transform.position);
-				}
-			}
-		}
-
-		private Vector2 FlipVertically(Vector2 vec)
-		{
-			return new Vector2(vec.x, Screen.height - vec.y);
 		}
 
 		private void Vision_OnRectanglesRecognized(object sender, RectanglesRecognizedArgs e)
@@ -190,70 +132,9 @@ namespace Examples
 			var rectangles = e.rectangles.OrderByDescending(entry => entry.area).ToList();
 			var found = false;
 
-			var hits = new List<ARRaycastHit>();
-
 			foreach (var rect in rectangles)
 			{
-				// The order is not correct
-				topLeft = Vector3.Scale(rect.bottomRight, ScreenDimensions);
-				bottomLeft = Vector3.Scale(rect.topRight, ScreenDimensions);
-				topRight = Vector3.Scale(rect.bottomLeft, ScreenDimensions);
-				bottomRight = Vector3.Scale(rect.topLeft, ScreenDimensions);
-
-				if (_raycastManager.Raycast(rect.topLeft, hits, TrackableType.Planes))
-				{
-					var result = hits.First();
-					if (_topLeftMarker == null)
-					{
-						_topLeftMarker = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-					}
-
-					_topLeftMarker.transform.localScale = Vector3.one * 0.1f;
-					_topLeftMarker.transform.position = result.pose.position;
-					_topLeftMarker.transform.rotation = result.pose.rotation;
-					Debug.Log("Top left marker pos" + _topLeftMarker.transform.position);
-				}
-
-				if (_raycastManager.Raycast(rect.topRight, hits, TrackableType.Planes))
-				{
-					var result = hits.First();
-					if (_topRightMarker == null)
-					{
-						_topRightMarker = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-					}
-
-					_topRightMarker.transform.localScale = Vector3.one * 0.1f;
-					_topRightMarker.transform.position = result.pose.position;
-					_topRightMarker.transform.rotation = result.pose.rotation;
-				}
-
-				if (_raycastManager.Raycast(rect.bottomLeft, hits, TrackableType.Planes))
-				{
-					var result = hits.First();
-					if (_bottomLeftMarker == null)
-					{
-						_bottomLeftMarker = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-					}
-
-					_bottomLeftMarker.transform.localScale = Vector3.one * 0.1f;
-					_bottomLeftMarker.transform.position = result.pose.position;
-					_bottomLeftMarker.transform.rotation = result.pose.rotation;
-				}
-
-				if (_raycastManager.Raycast(rect.bottomRight, hits, TrackableType.Planes))
-				{
-					var result = hits.First();
-					if (_bottomRightMarker == null)
-					{
-						_bottomRightMarker = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-					}
-
-					_bottomRightMarker.transform.localScale = Vector3.one * 0.1f;
-					_bottomRightMarker.transform.position = result.pose.position;
-					_bottomRightMarker.transform.rotation = result.pose.rotation;
-				}
-#if false
-				if (HitTestForRect(rect, out var result))
+				if (HitTestForRect(rect, out var hitResult))
 				{
 					if (_marker == null)
 					{
@@ -267,22 +148,19 @@ namespace Examples
 					}
 
 					// Assign the corners of the marker game object to the surface hit points
-					_marker.TopLeft = result.topLeft;
-					_marker.TopRight = result.topRight;
-					_marker.BottomRight = result.bottomRight;
-					_marker.BottomLeft = result.bottomLeft;
-
+					_marker.TopLeft = hitResult.topLeft;
+					_marker.TopRight = hitResult.topRight;
+					_marker.BottomRight = hitResult.bottomRight;
+					_marker.BottomLeft = hitResult.bottomLeft;
+				
 					found = true;
 				}
 			}
-
+			
 			if (_marker != null)
 			{
 				// Hide the marker if no rectangles were found
 				_marker.gameObject.SetActive(found);
-			}
-				
-#endif
 			}
 		}
 	}
